776a56bc4765d8ae4c971cad3d4aab5f5c814f42
Sousa Morais <vi.morais@yahoo.com>
2025-09-03 13:54:45 -0300

    feat: add sign up page

--- src/app/signup-user/page.tsx
+++ src/app/signup-user/page.tsx
@@ -3,8 +3,8 @@ import SignUpUserForm from "./signup-user-form";
 
 export default function SignUpUser() {
   return (
-    <div className="w-screen h-screen shadow-default bg-[url('/public/background-image.svg')] bg-cover bg-center bg-no-repeat px-30 py-5 font-poppins">
-      <div className="h-full backdrop-blur-md bg-white/25 shadow-2xl p-10 rounded-4xl flex flex-col items-center gap-5 overflow-y-scroll scrollbar-hide">
+    <div className="w-screen bg-[url('/public/background-image.svg')] bg-cover bg-center bg-no-repeat px-30 py-5 font-poppins">
+      <div className="h-full backdrop-blur-md bg-white/2 p-10 rounded-4xl flex flex-col items-center gap-5 overflow-y-scroll scrollbar-hide">
         <Title>Cadastro do cliente</Title>
         <SignUpUserForm />
       </div>
