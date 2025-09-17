e448241c888d4af28a4d2595f9c0f0f2bb27786b
Sousasz <vi.morais@yahoo.com>
2025-09-17 11:30:33 -0300

    feat: add form for reset password page

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,15 +1,27 @@
 import Image from "next/image";
 import petIconImage from "../../../public/pet-icon.webp";
+import MaskedInput from "@/components/shared/masked-input";
+import Touchable from "@/components/ui/touchable";
 
 export default function ResetPassword() {
   return (
-    <div className="sm:max-w-[90%] shadow-default h-[90%] bg-green-light bg-[url('/background-image.webp')] bg-cover bg-center bg-no-repeat">
-      <div className="backdrop-blur-md bg-white/25 shadow-2xl p-10 rounded-lg flex flex-col gap-10 overflow-y-auto scrollbar-hide">
-        <Image
-          src={petIconImage}
-          alt="Ícone de um cachorro - marca da clínica veterinária"
-        />
-      </div>
+    <div className="flex flex-col items-center w-full h-screen bg-green-light bg-[url('/background-image.webp')] bg-cover bg-center bg-no-repeat">
+      <Image
+        src={petIconImage}
+        alt="Ícone de um cachorro - marca da clínica veterinária"
+      />
+
+      <div className="flex flex-col gap-4">
+        <div className="flex flex-col gap-3">
+          <h2 className="font-semibold text-3xl text-center">Redefinir senha</h2>
+          <p>Adicione o seu telefone para enviarmos um link de redefinição de senha</p>
+        </div>
+
+        <form className="flex flex-col gap-8 m-5">
+          <MaskedInput placeholder="Telefone" mask="(99) 99999-9999" />
+          <Touchable>Enviar telefone</Touchable>
+        </form>
+      </div>  
     </div>
   );
 }
