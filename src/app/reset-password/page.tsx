06f1ebc12ad6b99b431868d7695514d0d93940e5
Sousa Morais <vi.morais@yahoo.com>
2025-10-11 20:38:55 -0300

    feat/add input otp toreset password

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -1,9 +1,22 @@
 import Image from "next/image";
 import petIconImage from "../../../public/images/pet-icon.webp";
 import MaskedInput from "@/components/shared/masked-input";
-import LinkConfirmationModal from "@/modals/link-confirmation";
+import { useState } from "react";
+import Touchable from "@/components/ui/touchable";
+import InputOTPValidation from "@/components/ui/input-otp-validation";
 
 export default function ResetPassword() {
+  const [isTelephoneOnDatabase, setIsTelephoneOnDatabase] = useState(false);
+  const [isCodeCheck, setIsCodeCheck] = useState(false);
+
+  function verifyTelephone() {
+    setIsTelephoneOnDatabase(true);
+  }
+
+  function verifyCodeOnSMS() {
+    setIsCodeCheck(true);
+  }
+
   return (
     <div className="flex flex-col justify-center items-center w-full h-screen bg-green-light bg-cover bg-center bg-no-repeat bg-[url('/images/background-image.webp')] gap-5">
       <Image
@@ -13,15 +26,40 @@ export default function ResetPassword() {
 
       <div className="flex flex-col gap-4">
         <div className="flex flex-col gap-3">
-          <h2 className="font-semibold text-3xl text-center">Redefinir senha</h2>
-          <p>Adicione o seu telefone para enviarmos um link de redefinição de senha</p>
+          <h2 className="font-semibold text-3xl text-center">
+            Redefinir senha
+          </h2>
         </div>
 
         <form className="flex flex-col gap-8 m-5">
-          <MaskedInput placeholder="Telefone" mask="(99) 99999-9999" />
-          <LinkConfirmationModal />
+          {isTelephoneOnDatabase ? (
+            <div className="flex flex-col gap-5">
+              <p>
+                Enviamos um código SMS para o telefone (xx) xxxxx-xxxx.
+                Verifique seu dispositivo
+              </p>
+              <InputOTPValidation />
+              <Touchable onClick={verifyCodeOnSMS}>Enviar código</Touchable>
+            </div>
+          ) : (
+            <div className="flex flex-col gap-5">
+              <p>
+                Adicione o seu telefone para enviarmos um link de redefinição de
+                senha
+              </p>
+              <MaskedInput placeholder="Telefone" mask="(99) 99999-9999" />
+              <Touchable onClick={verifyTelephone}>Enviar telefone</Touchable>
+            </div>
+          )}
+
+          <button
+            onClick={() => setIsTelephoneOnDatabase(false)}
+            className="underline cursor-pointer"
+          >
+            Voltar
+          </button>
         </form>
-      </div>  
+      </div>
     </div>
   );
 }
